14d97798c347a2088de0a169961183846fe98602
cc <798973262@qq.com>
2020-04-19 21:49:48 +0800

    :banana:start

---
+++ mm/js/shuju.js
@@ -0,0 +1,40 @@
+[
+    {
+        "firstChild": "首页",
+        "secondChild": []
+    },
+    {
+        "firstChild": "解决方案",
+        "secondChild": [
+            { "name": "智慧物业" },
+            { "name": "智慧商业" },
+            { "name": "智慧生活" },
+            { "name": "智慧城市" }
+        ]
+    },
+    {
+        "firstChild": "产品中心",
+        "secondChild": [
+            { "name": "千丁APP" },
+            { "name": "物业云" },
+            { "name": "物联网" },
+            { "name": "呼叫中心" },
+            { "name": "用户营业云" },
+            { "name": "丁管家" },
+            { "name": "丁老板" },
+            { "name": "千丁Live" }
+        ]
+    },
+    {
+        "firstChild": "招商合作",
+        "secondChild": []
+    },
+    {
+        "firstChild": "关于我们",
+        "secondChild": [
+            { "name": "千丁介绍" },
+            { "name": "新闻动态" },
+            { "name": "加入我们" }
+        ]
+    }
+]
